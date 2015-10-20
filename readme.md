fa229abc5a415fe2e430dcb8fad6a8e7ae46d90a
Tyler Getsay <tylergetsay@gmail.com>
2015-10-19 22:38:48 -0400

    .gitignore is now working

--- readme.md
+++
@@ -1,3 +0,0 @@
-## MakerSpace Manager
-
-This is a laravel application to manage a makerspace.
--- resources/views/errors/404.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.base')
-@section('content')
-
-
-    <h1>Nothing to see here</h1>
-
-@endsection
