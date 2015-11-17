38fcecd2e1411657e7931c934d3662aea409ee7a
Tyler Getsay <tylergetsay@gmail.com>
2015-11-17 01:02:07 -0500

    Require auth to view makerspace internals

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -13,6 +13,8 @@ class PermissionController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth');
+
     }
 
     /**
--- app/Http/Controllers/TransactionTypeController.php
+++ app/Http/Controllers/TransactionTypeController.php
@@ -13,6 +13,7 @@ class TransactionTypeController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth');
     }
 
     /**
