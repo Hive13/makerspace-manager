d79dfd9787e8b0a6f37287ddba828ac6a1a1b30c
Tyler Getsay <tylergetsay@gmail.com>
2015-11-30 20:58:29 -0500

    Add missing file whoops

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+	namespace App\Http\Requests;
+
+	class UpdateProfileRequest extends Request {
+
+		/**
+		 * Determine if the user is authorized to make this request.
+		 *
+		 * @return bool
+		 */
+		public function authorize() {
+
+			return true;
+		}
+
+		/**
+		 * Get the validation rules that apply to the request.
+		 *
+		 * @return array
+		 */
+		public function rules() {
+
+			return ['file' => 'mimes:jpeg,bmp,png'];
+		}
+	}
